ed78c61d487fac14a7144fb60814d2f36790b04a
SaiKiran93814 <teppali123@gmail.com>
2025-07-25 16:28:41 +0530

    changed layout file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,30 @@
-import { Link } from 'react-router-dom';
-import { categories } from '../data/categories';
+// import { Link } from 'react-router-dom';
+// import { categories } from '../data/categories';
+// import React from 'react';
+
+// const Layout = ({ children }) => {
+//   return (
+//     <div>
+//       <h1>Shop Categories</h1>
+//       <nav>
+//         <ul>
+//           {categories.map((cat) => (
+//             <li key={cat.id}>
+//               <Link to={`/${cat.id}`} data-testid={`link-${cat.id}`}>{cat.name}</Link>
+//             </li>
+//           ))}
+//         </ul>
+//       </nav>
+//       <hr />
+//       {children}
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Layout = ({ children }) => {
   return (
@@ -8,11 +32,12 @@ const Layout = ({ children }) => {
       <h1>Shop Categories</h1>
       <nav>
         <ul>
-          {categories.map((cat) => (
-            <li key={cat.id}>
-              <Link to={`/${cat.id}`} data-testid={`link-${cat.id}`}>{cat.name}</Link>
-            </li>
-          ))}
+          <li>
+            <Link to="/women">Women</Link>
+          </li>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
         </ul>
       </nav>
       <hr />

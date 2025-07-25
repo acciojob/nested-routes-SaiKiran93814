9957ca5fd96f111f9628464bfd0e436b8989665b
SaiKiran93814 <teppali123@gmail.com>
2025-07-25 16:12:18 +0530

    modified layout file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,18 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { categories } from '../data/categories';
 
 const Layout = ({ children }) => {
   return (
     <div>
-      {/* <h1>Shop Categories</h1> */}
+      <h1>Shop Categories</h1>
       <nav>
-        {categories.map((cat) => (
-          <Link key={cat.id} to={`/${cat.id}`} style={{ marginRight: '10px' }}>
-            {cat.name}
-          </Link>
-        ))}
+        <ul>
+          {categories.map((cat) => (
+            <li key={cat.id}>
+              <Link to={`/${cat.id}`}>{cat.name}</Link>
+            </li>
+          ))}
+        </ul>
       </nav>
       <hr />
       {children}

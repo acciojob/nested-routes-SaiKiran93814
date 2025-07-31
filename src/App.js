b1dea68ec18c30ecd3d12cc9923e2684ddb1466e
SaiKiran93814 <teppali123@gmail.com>
2025-07-31 22:41:42 +0530

    changed the package file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout';
 import Category from './components/Category';
-import ItemDetail from './components/ItemDetail';
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route exact path="/" component={() => <h2>Index</h2>} />
-           <Route path="/:categoryId/:itemId?" component={Category} />
+          <Route exact path="/" component={() => (
+            <div>
+              <h2>Welcome to Our Store</h2>
+              <p>Select a category above.</p>
+            </div>
+          )} />
+          <Route path="/:categoryId/:itemId?" component={Category} />
         </Switch>
       </Layout>
     </Router>
